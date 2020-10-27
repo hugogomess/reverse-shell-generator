9615ad55c39bb226c3dec6d0806ca2e3b4a3f3b6
hugogomess <hugogomes02@gmail.com>
2020-10-27 20:29:16 -0300

    add argparse and arguments

--- reverse_shell_generator.py
+++ reverse_shell_generator.py
@@ -3,6 +3,7 @@
 # I don't believe in license.
 # You can do whatever you want with this tool.
 
+import argparse
 import colorama
 from colorama import Fore, Style
 
@@ -21,3 +22,47 @@ def banner():
     """ + Style.RESET_ALL)
 
 banner()
+
+parser = argparse.ArgumentParser(usage="python3 reverse_shell_generator.py [-i/--host] HOST [-p/--port] PORT [OPTIONS]")
+optional = parser._action_groups.pop()
+required = parser.add_argument_group('required arguments')
+
+required.add_argument("-i","--host",help="host listening the connection")
+required.add_argument("-p","--port",help="port listening the connection")
+optional.add_argument("-t","--type",help="language or tool - Options: bash, nc, python, python3, php, ruby, perl")
+optional.add_argument("-s","--shell",help="shell - Options: sh, bash")
+optional.add_argument("-e","--encode",help="output encode - Options: base64, urlencode, hex")
+
+parser._action_groups.append(optional)
+args = parser.parse_args()
+
+if (args.host):
+    _host = args.host
+else:
+    _host = None
+
+if (args.port):
+    _port = args.port
+else:
+    _port = None
+
+if (args.type):
+    _type = args.type
+else:
+    _type = None
+
+if (args.shell):
+    _shell = args.shell
+else:
+    _shell = None
+
+if (args.encode):
+    _encode = args.encode
+else:
+    _encode = None
+
+if (not _host) or (not _port):
+    print("Incorrect usage")
+    print("Usage: python3 reverse_shell_generator.py [-i/--host] HOST [-p/--port] PORT [OPTIONS]")
+else:
+    print("Tool execution will be here")
